eb52284c8fa649097cfe99b02c454e679b0987a2
GusSouzaCastro <160952085+GusSouzaCastro@users.noreply.github.com>
2024-02-22 21:06:39 -0300

    Update and rename Fibonacci.py to Fibonacci/Fibonacci_old.py

---
+++ Fibonacci/Fibonacci_old.py
@@ -0,0 +1,11 @@
+print(1)
+a=1
+b=1
+n=int(1)
+while n<101:
+ print (a)
+ a=a+b
+ b=a-b
+ '''cria uma relação/descrição cíclica entre "a" e "b", onde o resultado
+ de um depende do valor de outro, que voltará a depender do mesmo.'''
+ n=n+1
